36054000b84560d5490dc9abfd4dcb6ea021182b
lopatin <lopatin@krista.ru>
2023-08-10 09:05:09 +0300

    add component best_friends in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import styles from './Navbar.module.css'
 import { NavLink } from "react-router-dom";
+import { BestFriends } from "./bestFriends/BestFriends";
 
 export const Navbar = () => {
     return (
         <nav className={styles.nav}>
             <div>
-                <NavLink to='/profile' className={ e => e.isActive ? styles.active : styles.item}>Profile</NavLink>
+                <NavLink to='/profile' className={e => e.isActive ? styles.active : styles.item}>Profile</NavLink>
             </div>
             <div>
-                <NavLink to='/dialogs' className={ e => e.isActive ? styles.active : styles.item}>Massage</NavLink>
+                <NavLink to='/dialogs' className={e => e.isActive ? styles.active : styles.item}>Massage</NavLink>
             </div>
             <div>
-                <NavLink to='/news' className={ e => e.isActive ? styles.active : styles.item}>News</NavLink>
+                <NavLink to='/news' className={e => e.isActive ? styles.active : styles.item}>News</NavLink>
             </div>
             <div>
-                <NavLink to='/music' className={ e => e.isActive ? styles.active : styles.item}>Music</NavLink>
+                <NavLink to='/music' className={e => e.isActive ? styles.active : styles.item}>Music</NavLink>
             </div>
             <div>
-                <NavLink to='/settings' className={ e => e.isActive ? styles.active : styles.item}>Settings</NavLink>
+                <NavLink to='/settings' className={e => e.isActive ? styles.active : styles.item}>Settings</NavLink>
             </div>
+            <BestFriends/>
         </nav>
     )
 }
