2c3409379235103e89047fac540c6879dbc03b84
Andrey L <lopatkin.andrushka@yandex.ru>
2024-02-17 14:59:09 +0300

    second commit

--- src/components/music/Music.jsx
+++ src/components/music/Music.jsx
@@ -1,9 +1,71 @@
-// import styles from './Dialogs.module.css'
+import styles from './Music.module.css'
+
+const data1 = [
+    { name: 'Контроль региональных проектов', section: 'Подраздел 1', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 2', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 3', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 4', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 5', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 6', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 7', plan: 0, fact: 0 },
+]
+const data2 = [
+    { name: '', section: 'Раздел 2', plan: 0, fact: 0 },
+    { name: '', section: 'Раздел 3', plan: 0, fact: 0 },
+    { name: '', section: 'Раздел 4', plan: 0, fact: 0 },
+    { name: '', section: 'Раздел 5', plan: 0, fact: 0 },
+]
+const data3 = [
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+    { plan: 0, fact: 0 },
+]
+const renderSection = (data) => {
+    return (<div className={styles.shapeSection}>
+        <div className={styles.nameSection}>
+            <div className={styles.nameStyle}>«{data.name}»</div>
+            <div className={styles.grayStyle}>{data.section}</div>
+        </div>
+    </div>)
+}
+const renderSection2 = (data) => {
+    return (<div className={styles.shapeSection}>
+        <div className={styles.valueSection}>
+            <div className={styles.value}>План</div>
+            <div className={styles.value}>{data.plan}</div>
+            <div className={styles.value}>Факт</div>
+            <div className={styles.value}>{data.fact}</div>
+        </div>
+    </div>)
+}
+console.time('method1')
 
 export const Music = (props) => {
     return (
-        <div>
-            dialogs
+        <div className={styles.container}>
+            <div className={styles.container1}>
+                <div className={styles.nameStyle}>«Контроль региональных проектов»</div>
+                <div className={styles.grayStyle}>Раздел 1</div>
+            </div>
+            <div className={styles.container2}>
+                {data1.map((e) => renderSection(e))}
+            </div>
+            <div className={styles.container3}>
+                {data2.map((e) => renderSection(e))}
+            </div>
+            <div className={styles.container4}>
+                {data3.map((e) => renderSection2(e))}
+            </div>
         </div>
     )
 }
+
+console.timeEnd('method1')
--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,9 +1,68 @@
-// import styles from './Dialogs.module.css'
+import styles from './News.module.css'
+
+const data1 = [
+    { name: '', section: 'Подраздел 1', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 2', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 3', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 4', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 5', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 6', plan: 0, fact: 0 },
+    { name: '', section: 'Подраздел 7', plan: 0, fact: 0 },
+]
+const data2 = [
+    { name: '', section: 'Раздел 2', plan: 0, fact: 0 },
+    { name: '', section: 'Раздел 3', plan: 0, fact: 0 },
+    { name: '', section: 'Раздел 4', plan: 0, fact: 0 },
+    { name: '', section: 'Раздел 5', plan: 0, fact: 0 },
+]
+
+// export const News = (props) => {
+//     return (
+        // <div className={styles.container}>
+        //     <div className={styles.container1}>1111</div>
+        //     <div className={styles.container2}>
+        //         {data1.map((e) => renderSection(e))}
+        //     </div>
+        //     <div className={styles.container3}>
+        //         {data2.map((e) => renderSection(e))}
+        //     </div>
+        // </div>
+//     )
+// }
 
 export const News = (props) => {
     return (
-        <div>
-            dialogs
+        <div className={styles.box}>
+            <div className={styles.item}>Lorem.</div>
+            <div className={styles.item}>Lorem, ipsum.</div>
+            <div className={styles.item}>Lorem, ipsum dolor.</div>
+            <div className={styles.item}>Lorem ipsum dolor sit amet.</div>
+            <div className={styles.item}>Lorem.</div>
+            <div className={styles.item}>Lorem, ipsum.</div>
+            <div className={styles.item}>Lorem, ipsum dolor.</div>
+            <div className={styles.item}>Lorem ipsum dolor sit amet.</div>
+            <div className={styles.item}>Lorem.</div>
+            <div className={styles.item}>Lorem, ipsum.</div>
+            <div className={styles.item}>Lorem, ipsum dolor.</div>
+            <div className={styles.item}>Lorem ipsum dolor sit amet.</div>
+            <div className={styles.item}>Lorem.</div>
+            <div className={styles.item}>Lorem, ipsum.</div>
+            <div className={styles.item}>Lorem, ipsum dolor.</div>
+            <div className={styles.item}>Lorem ipsum dolor sit amet.</div>
         </div>
     )
+}
+
+const renderSection = (data) => {
+
+    return (<div className={styles.shapeSection}>
+        <div className={styles.nameSection}>
+            <div>{data.nameSection}</div>
+            <div>{data.section}</div>
+        </div>
+        <div className={styles.valueSection}>
+            <div>{data.plan}</div>
+            <div>{data.fact}</div>
+        </div>
+    </div>)
 }
