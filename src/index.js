bbb43db2b1abcfeb86ea819f9a48f0917fe72f9d
Andrey L <lopatkin.andrushka@yandex.ru>
2024-02-21 23:06:11 +0300

    local redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './redux/reduxStore'
+import { store } from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 export const renderEntireTree = (state) => {
