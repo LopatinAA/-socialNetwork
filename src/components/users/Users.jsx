704a0a582ecc2140ed6af340dd2bfe2497fc8ce2
Andrey L <lopatkin.andrushka@yandex.ru>
2024-03-07 21:32:17 +0300

    Users API

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import styles from './Users.module.css'
+import axios from "axios";
 
 export const Users = (props) => {
 
+    // axios.get('https://social-network.samuraijs.com/api/1.0/users').then( response => {
+    //     props.setUsers(response.data.items);
+    // })
+
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, followed: false,  photoUrl: 'https://avatars.mds.yandex.net/i?id=6c854abc6588200fccc86aff11e66a86997effb4-9053276-images-thumbs&n=13', fullName: 'Dmitry', status: 'im a boss', location: { city: 'minsk', country: 'Belarus' } },
-            { id: 2, followed: true,  photoUrl: 'https://avatars.mds.yandex.net/i?id=6c854abc6588200fccc86aff11e66a86997effb4-9053276-images-thumbs&n=13', fullName: 'Oleg', status: 'im a', location: { city: 'minsk', country: 'Belarus' } },
-            { id: 3, followed: true,  photoUrl: 'https://avatars.mds.yandex.net/i?id=6c854abc6588200fccc86aff11e66a86997effb4-9053276-images-thumbs&n=13', fullName: 'Anton', status: 'im', location: { city: 'minsk', country: 'Belarus' } },
-            { id: 4, followed: false,  photoUrl: 'https://avatars.mds.yandex.net/i?id=6c854abc6588200fccc86aff11e66a86997effb4-9053276-images-thumbs&n=13', fullName: 'Mark', status: '', location: { city: 'minsk', country: 'Belarus' } },
-    
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then( response => {
+        props.setUsers(response.data.items);
+    })
     } 
 
     return (
@@ -32,12 +33,12 @@ export const Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{user.fullName}</div>
+                        <div>{user.name}</div>
                         <div>{user.status}</div>
                     </span>
                     <span>
-                        <div>{user.location.country}</div>
-                        <div>{user.location.city}</div>
+                        {/* <div>{user.location.country}</div>
+                        <div>{user.location.city}</div> */}
                     </span>
                 </span>
             </div>)}
